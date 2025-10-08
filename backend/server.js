812b2944a1d56648b944e0ea84e5fe6db71669a1
jay2006-dev <vijaythimmishetty999@gmail.com>
2025-10-08 10:47:11 +0530

    new changes

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,21 @@ import express from 'express'
 import Razorpay from 'razorpay'
 import cors from 'cors'
 import crypto from 'crypto'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
+const RAZORPAY_KEY_ID = import.meta.env.RAZORPAY_KEY_ID
+const RAZORPAY_KEY_SECRET = import.meta.env.RAZORPAY_KEY_SECRET
+
 // ✅ Your Razorpay credentials
 const razorpay = new Razorpay({
-  key_id: 'rzp_test_RGeGMOEnLzUqYw',
-  key_secret: 'napLniZkPKSCZSKFXKWXvDle',
+  key_id: RAZORPAY_KEY_ID,
+  key_secret: RAZORPAY_KEY_SECRET,
 })
 
 // ✅ Create order route (with cart calculation)
